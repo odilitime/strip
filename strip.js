6e58eb969d6986b77d5e8a511d066067abf02ad4
Odilitime <janesmith@airmail.cc>
2019-02-05 01:27:27 -0800

    DRY

--- strip.js
+++ strip.js
@@ -1,91 +1,7 @@
-const frequency = require('./lib/frequency_en.js')
+const language_frequency = require('./lib/frequency_en.js')
+const charset2homophones = require('./lib/generator.js')
 const encrypt = require('./lib/encrypt.js')
 const decrypt = require('./lib/decrypt.js')
-
-// borrowed from https://stackoverflow.com/a/6940176
-function ArrNoDupe(a) {
-  var temp = {}
-  for (var i = 0; i < a.length; i++)
-    temp[a[i]] = true
-  var r = []
-  for (var k in temp)
-    r.push(k)
-  return r
-}
-
-// borrowed from https://stackoverflow.com/a/6274381
-function shuffle(a) {
-  var j, x, i
-  for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1))
-    x = a[i]
-    a[i] = a[j]
-    a[j] = x
-  }
-  return a
-}
-
-// generate a set of homephones based on a range and charset
-function charset2homophones(charset, max) {
-  if (max === undefined) {
-    console.error('max is required')
-    return {}
-  }
-  noDupeCharset = ArrNoDupe(charset)
-  var rows = noDupeCharset.length
-  if (max < rows) {
-    console.error('max(', max, ') needs to exceed unique characters (', rows, ')')
-    return {}
-  }
-
-  // total slots
-  var totalFreqSlots = 0
-  var slotMap = {}
-  for(var i in noDupeCharset) {
-    var l = noDupeCharset[i]
-    var freq = frequency[l] + 1
-    for(var j = totalFreqSlots; j < totalFreqSlots + freq; j++) {
-      slotMap[j] = l
-    }
-    totalFreqSlots += freq
-  }
-
-  var l2h_map = {}
-  var last = 0
-
-  var allButOne = max - noDupeCharset.length
-  // random distribution
-  for(var i=0; i < allButOne; i++) {
-    var randSlot = Math.floor(Math.random() * totalFreqSlots)
-    var randLetter = slotMap[randSlot]
-    //if (randLetter === undefined) console.error('no slot at', randSlot, '/', totalFreqSlots)
-    if (l2h_map[randLetter] === undefined) l2h_map[randLetter] = []
-    l2h_map[randLetter].push(last) // enumerate value
-    last++
-  }
-
-  // make sure all letters get at least one assignment
-  var shuffledLetters = shuffle(noDupeCharset)
-  for(var i in shuffledLetters) {
-    var l = shuffledLetters[i]
-    if (l2h_map[l] === undefined) {
-      l2h_map[l] = [last] // enumerate value
-      last++
-    }
-  }
-
-  console.log('final round', last, '/', max)
-  // final round: random sprinkle
-  while(last < max) {
-    var randSlot = Math.floor(Math.random() * totalFreqSlots)
-    var randLetter = slotMap[randSlot]
-    l2h_map[randLetter].push(last) // enumerate value
-    last++
-  }
-
-  console.log('used', last, '/', max)
-  return l2h_map
-}
 
 var our_charset = [
   'a',
@@ -188,7 +104,7 @@ var our_charset = [
   '\t',
 ]
 
-var homophones = charset2homophones(our_charset, 394)
+var homophones = charset2homophones(language_frequency, our_charset, 394)
 console.dir(homophones)
 var test = encrypt('The job requires extra pluck and zeal from every young wage earner. ', homophones)
 console.log(test)
